28efe0e330eb55fb63966a8f2919e1e0cbd2a85e
ambruhsia <rajputamrita54@gmail.com>
2024-11-01 17:55:34 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,16 +1,49 @@
-import socket  # noqa: F401
+import socket
 
+class KafkaHandler:
+    def __init__(self, correlation_id: int = 7):
+        self.correlation_id = correlation_id
+
+    def get_correlation_id_bytes(self):
+        correlation_id_bytes = self.correlation_id.to_bytes(4, byteorder="big")
+        return correlation_id_bytes
+
+    def get_headers(self):
+        correlation_id = self.get_correlation_id_bytes()
+        header = correlation_id
+        return header
+
+    @staticmethod
+    def get_message_size():
+        message_size = int(4).to_bytes(4, byteorder="big")
+        return message_size
+
+    def get_response(self):
+        message_size = self.get_message_size()
+        correlation_id = self.get_correlation_id_bytes()
+        response = message_size + correlation_id
+        return response
 
 def main():
-    # You can use print statements as follows for debugging,
-    # they'll be visible when running tests.
     print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
+    
+    # Create the server
     server = socket.create_server(("localhost", 9092), reuse_port=True)
-    server.accept() # wait for client
+    print("Server is listening on port 9092...")
 
+    while True:
+        client, addr = server.accept()  # wait for client
+        print(f"Connection from {addr} accepted.")
+        
+        data = client.recv(1024)  # Receive data from the client
+        if data:
+            print(f"Received data: {data}")
+            response = KafkaHandler(correlation_id=7).get_response()
+            client.sendall(response)
+            print(f"Sent response: {response}")
+        
+        client.close()
+        print(f"Connection from {addr} closed.")
 
 if __name__ == "__main__":
     main()
